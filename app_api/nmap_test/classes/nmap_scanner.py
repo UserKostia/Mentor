96b040f17506ec78633dbaba2fd7fae095e967ba
UserKostia <kostastriletski@gmail.com>
2024-07-12 19:14:06 +0300

    Added comments to nmap_test folder

--- app_api/nmap_test/classes/nmap_scanner.py
+++ app_api/nmap_test/classes/nmap_scanner.py
@@ -2,12 +2,48 @@ from subprocess import run
 
 
 class NmapScanner:
+    """
+    A class to represent a Nmap scanner.
+
+    Attributes
+    ----------
+    ip : str
+        The IP address to scan.
+    nmap : str
+        The path to the Nmap executable.
+    options : list[str]
+        A list of options to pass to the Nmap command.
+    """
+
     def __init__(self, ip: str, nmap: str, options: list[str]):
+        """
+        Constructs all the necessary attributes for the NmapScanner object.
+
+        Parameters:
+        ----------
+        :param ip:
+            The IP address to scan.
+        :param nmap:
+            The path to the Nmap executable.
+        :param options:
+            A list of options to pass to the Nmap command.
+        """
         self.ip = ip
         self.nmap = nmap
         self.options = options
 
     def scan(self) -> str:
+        """
+        Runs the Nmap scan with the provided options on the given IP address.
+
+        :return: str
+            The output of Nmap scan.
+        """
+        # Constract the command to run with the provided options and IP address
         command = [self.nmap] + self.options + [self.ip]
+
+        # Execute the command and capture the output.
         result = run(command, capture_output=True, text=True)
+
+        # Return the standard output from the Nmap command
         return result.stdout
--- app_api/nmap_test/main.py
+++ app_api/nmap_test/main.py
@@ -1,22 +1,33 @@
 from json import dumps
 from xmltodict import parse
-from classes.nmap_scanner import *
+from classes.nmap_scanner import NmapScanner
 
 
+# Default IP to scan
 ip = "94.243.121.4"
+
+# Path to the Nmap executable
 nmap_path = 'C:\\Program Files (x86)\\Nmap\\nmap.exe'
+
+# Some options to scan IP address
 options = ["-sS", "-sU", "-sV", "-vv", "-F", "-oX", "nmap_result\\results.xml"]
 
 
+# Creates object and scans it
 nmap_test = NmapScanner(ip, nmap_path, options)
 result = nmap_test.scan()
 print(f"{result = }")
 
 
+# Open the resulting XML file and read its content
 with open("nmap_result\\results.xml", "r") as f:
     xml_file = f.read()
 
+
+# Converts the XML file content to a dictionary
 xml_dict = parse(xml_file)
 
+
+# Shows results in a JSON-like format with indentation
 print("\nXML TO DICT:")
 print(dumps(xml_dict, indent=4))
